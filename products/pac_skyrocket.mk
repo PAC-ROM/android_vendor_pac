caab3dc8ad7f7ee925c340fa5e0d13f9b9491917
gs2usr <jglarios1@gmail.com>
2012-11-09 04:44:36 -0800

    aenlav?>,s;ldvm

---
+++ products/pac_skyrocket.mk
@@ -0,0 +1,26 @@
+# Check for target product
+ifeq (pac_skyrocket,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
+# AOKP device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/skyrocket
+
+# PAC device overlay
+$(shell cp -f vendor/pac/overlay/pac/samsung/hdpi/frameworks/base/core/res/assets/images/android-logo-mask.png frameworks/base/core/res/assets/images/android-logo-mask.png)
+
+# PAC boot logo
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/logo.rle
+
+# include ParanoidAndroid common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/samsung/skyrocket/cm.mk)
+
+PRODUCT_NAME := pac_skyrocket
+
+
+endif
