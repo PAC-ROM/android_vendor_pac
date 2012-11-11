59138c13bad73af35d9b650ff7553c6473b5a295
Nikola Majkic <n1kolaa@hotmail.com>
2012-11-11 17:34:55 +0100

    Adding Anzu

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@ add_lunch_combo pac_smultron-userdebug
 add_lunch_combo pac_urushi-userdebug
 add_lunch_combo pac_zeus-userdebug
 add_lunch_combo pac_zeusc-userdebug
+
 # Galaxy s2 Series
 add_lunch_combo pac_hercules-userdebug
 add_lunch_combo pac_i9100-userdebug
