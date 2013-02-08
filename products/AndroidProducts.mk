a6c46d66dcd5cb2b0a1c9d10acda53e6ef9d9231
Faradis <DrubO@ovi.com>
2013-02-08 23:58:59 +0600

    haida P.A.C 4.2.1
    haida P.A.C 4.2.1

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -4,3 +4,6 @@ endif
 ifeq (pac_urushi,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pac_urushi.mk
 endif
+ifeq (pac_urushi,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pac_haida.mk
+endif
