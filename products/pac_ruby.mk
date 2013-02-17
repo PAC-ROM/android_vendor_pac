86529f3176f7745b409941ca45ee7d30a5a775bc
Alexander Pohl <alex.pohl1998@live.de>
2013-02-17 02:14:00 +0100

    add ruby depencies

--- products/pac_ruby.mk
+++ products/pac_ruby.mk
@@ -22,4 +22,9 @@ $(call inherit-product, device/htc/ruby/cm.mk)
 
 PRODUCT_NAME := pac_ruby
 
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+
+
 endif
