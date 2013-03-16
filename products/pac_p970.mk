656175e5c73e1a351c315400060e25c4d824b644
Bernhard <berniroth@aol.com>
2013-03-16 14:57:28 +0100

    add bootanimation to p920,p970 and crespo

--- products/pac_p970.mk
+++ products/pac_p970.mk
@@ -10,6 +10,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common
 # PAC device overlay
 $(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-mask_lg-hdpi.png frameworks/base/core/res/assets/images/android-logo-mask.png)
 
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
 # include ParanoidAndroid common configuration
 include vendor/pac/config/pac_common.mk
 
