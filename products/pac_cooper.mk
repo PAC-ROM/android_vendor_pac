779defc9625214c924a6c30f8d0c574d19437d04
Alexander Pohl <alex.pohl1998@live.de>
2013-03-16 16:56:12 +0100

    Update pac_cooper.mk

--- products/pac_cooper.mk
+++ products/pac_cooper.mk
@@ -9,7 +9,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common
 
 # PAC device overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/mdpi
-$(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-mask_samsung-mdpi.png frameworks/base/core/res/assets/images/android-logo-mask.png)
 
 # PAC boot logo
 PRODUCT_COPY_FILES += \
@@ -26,5 +25,9 @@ PRODUCT_NAME := pac_cooper
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/mdpi/bootanimation.zip:system/media/bootanimation.zip
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
 
 endif
