8a341d6468cf750af61a4075c305b946502009f6
Faradis <DrubO@ovi.com>
2013-03-26 12:36:40 +0600

    Added Pac bootanimation
    Added Pac bootanimation

--- products/pac_haida.mk
+++ products/pac_haida.mk
@@ -15,6 +15,10 @@ $(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-ma
 PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/common/bootlogo/sony-pac_480x854.rle:root/logo.rle
 
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
 # include ParanoidAndroid common configuration
 include vendor/pac/config/pac_common.mk
 
