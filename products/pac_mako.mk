e744b780f959d601cbd087b91edbd5aa441456b0
Tom Marshall <tdm.code@gmail.com>
2013-04-15 20:55:11 +0000

    Rework navbar support
    Most devices have hardware buttons but some do not, and need a navbar.
    
    Support this in a generic and consistent way:
    
    * Set com.android.systemui.statusbar.dpi=0 for default configs.
    
    * Introduce pa_nav_*.conf for devices that want navbar by default.
    
    * Set mako to use navbar by default.
    
    Device maintainers, please switch your devices to use pa_nav_*.conf as
    needed.  If there are any hdmi/mdpi/ldpi devices that want/need a navbar
    by default, copy the generic config to pa_nav_*.conf and set the navbar
    config appropriately.
    
    Change-Id: Ief2306c42ecc407e9d0e091076c3755560d85c44

--- products/pac_mako.mk
+++ products/pac_mako.mk
@@ -2,7 +2,7 @@
 ifeq (pac_mako,$(TARGET_PRODUCT))
 
 # OVERLAY_TARGET adds overlay asset source
-OVERLAY_TARGET := pa_xhdpi
+OVERLAY_TARGET := pa_nav_xhdpi
 
 # AOKP device overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/mako
