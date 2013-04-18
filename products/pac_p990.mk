0e09d456fd958f5c52c6f89cff63c8cd470936eb
metaiiica <digixp2006@googlemail.com>
2013-04-18 22:48:33 +0300

    Update pac_p990.mk

--- products/pac_p990.mk
+++ products/pac_p990.mk
@@ -10,6 +10,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common
 # PAC device overlay
 $(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-mask_lg-hdpi.png frameworks/base/core/res/assets/images/android-logo-mask.png)
 
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
 
@@ -17,5 +21,9 @@ include vendor/pac/config/pac_common.mk
 $(call inherit-product, device/lge/p990/cm.mk)
 
 PRODUCT_NAME := pac_p990
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
 
 endif
