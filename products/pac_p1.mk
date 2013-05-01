a3c18dcf8574fa9740a1bb961c8973922400d114
Alexander Pohl <alex.pohl1998@live.de>
2013-05-01 13:52:40 +0300

    copy hdpi bootanimatiom

--- products/pac_p1.mk
+++ products/pac_p1.mk
@@ -7,6 +7,10 @@ OVERLAY_TARGET := pa_hdpi
 # AOKP device overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common
 
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
 # PAC device overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/hdpi
 $(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-mask_samsung-hdpi.png frameworks/base/core/res/assets/images/android-logo-mask.png)
