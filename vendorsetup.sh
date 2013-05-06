c76e83dcb27c62508f890d070935f50649fb56b3
Sniper- <markp.mckinney@gmail.com>
2013-05-05 17:36:03 -0700

    Add m7 (HTC One) and m7wls (Sprint HTC One) support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -51,6 +51,8 @@ add_lunch_combo pac_saga-userdebug
 add_lunch_combo pac_jewel-userdebug
 add_lunch_combo pac_evita-userdebug
 add_lunch_combo pac_ville-userdebug
+add_lunch_combo pac_m7-userdebug
+add_lunch_combo pac_m7wls-userdebug
 #Samsung 2010
 add_lunch_combo pac_epicmtd-userdebug
 # Samsung 2013
