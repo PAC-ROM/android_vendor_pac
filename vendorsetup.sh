def830f080942149a97e767d17c63dcabd913528
apascual89 <apascual89@gmail.com>
2013-06-26 17:25:17 -0400

    m7att support
    Change-Id: I1188e851ad7270b527f8190910f9fe03495f2c37

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -63,6 +63,7 @@ add_lunch_combo pac_jewel-userdebug
 add_lunch_combo pac_evita-userdebug
 add_lunch_combo pac_ville-userdebug
 add_lunch_combo pac_m7-userdebug
+add_lunch_combo pac_m7att-userdebug
 add_lunch_combo pac_m7wls-userdebug
 # Samsung 2010
 add_lunch_combo pac_epicmtd-userdebug
