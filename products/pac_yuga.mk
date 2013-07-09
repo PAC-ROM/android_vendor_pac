65f7d6d104d406ba71804ff502d264c4a35dbfc7
jeroenqui <jeroenqui@gmail.com>
2013-07-09 15:06:39 +0200

    add xxhdpi bootanimation, switch yuga to xxhdpi bootanimation, switch yuga to xxhdpi PA profile and add yuga to nightlies
    Change-Id: Ieb2b8296a51388e112a06f08a0fce20f830dc33e

--- products/pac_yuga.mk
+++ products/pac_yuga.mk
@@ -2,7 +2,7 @@
 ifeq (pac_yuga,$(TARGET_PRODUCT))
 
 # OVERLAY_TARGET adds overlay asset source
-OVERLAY_TARGET := pa_yuga
+OVERLAY_TARGET := pa_xxhdpi
 
 # AOKP device overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/yuga
@@ -16,7 +16,7 @@ PRODUCT_COPY_FILES += \
 
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
+    vendor/pac/prebuilt/xxhdpi/bootanimation.zip:system/media/bootanimation.zip
 
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
