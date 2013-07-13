1886832ee1dd79efad48744df33479b1d32faba7
Ravi Kiran P <ravikirancg1@gmail.com>
2013-07-13 22:16:31 +0530

    Add support for edison
    Change-Id: I3544495fa4e823d104fea9cea8dbf11187e8267c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -119,6 +119,7 @@ add_lunch_combo pac_p930-userdebug
 add_lunch_combo pac_su640-userdebug
 add_lunch_combo pac_vs920-userdebug
 # Motorola 2013
+add_lunch_combo pac_edison-userdebug
 add_lunch_combo pac_mb886-userdebug
 add_lunch_combo pac_vanquish-userdebug
 add_lunch_combo pac_spyder-userdebug
