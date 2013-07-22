b902bafa962bf926ddc520afbff24c608097a9c7
Mike <gchild320@gmail.com>
2013-07-22 07:19:53 -0500

    clean up tf300t.mk and update contributors
    Change-Id: Ibf35e722be9b31abf4a88e504afdb1fccc1c0809

--- products/pac_tf300t.mk
+++ products/pac_tf300t.mk
@@ -5,22 +5,17 @@ ifeq (pac_tf300t,$(TARGET_PRODUCT))
 OVERLAY_TARGET := pa_tf300t
 
 # AOKP device overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/tf300t
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common_tablet
 
 # PAC device overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/mdpi
 
 # PA device overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/pa_tf700t
-
-# PAC boot logo
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/bootlogo/pac_logo_720x1280.rle:root/logo.rle
+PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/pa_tf300t
 
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/1280x800/bootanimation.zip:system/media/bootanimation.zip
+    vendor/pac/prebuilt/mdpi/bootanimation.zip:system/media/bootanimation.zip
 
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
