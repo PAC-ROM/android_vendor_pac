c9244f651da9e32e03e82084157c249a7e811864
Tianrui Guo <tianruig@gmail.com>
2013-08-01 06:32:30 +0200

    10.2 bringup for the galaxy player 5
    Change-Id: Id7e394b31c117d19e9009684bc4f356df71ef209

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -103,7 +103,7 @@ add_lunch_combo pac_l900-userdebug
 add_lunch_combo pac_t0lte-userdebug
 add_lunch_combo pac_n7000-userdebug
 add_lunch_combo pac_n7100-userdebug
-add_lunch_combo pac_venturi_usa-userdebug
+add_lunch_combo pac_venturi-userdebug
 add_lunch_combo pac_ypg1-userdebug
 # Samsung & LGE Legacy Devices
 add_lunch_combo pac_ancora-userdebug
