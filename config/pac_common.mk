fb42b021164b69fe2dfff1e571960c07e3f163d8
bluefa1con <bluefa1con@live.com>
2013-08-04 16:23:04 +0300

    Update to match new naming standard
    Change-Id: I3b8f7d78be6bebe780f5632914d3149f1d68787e

--- config/pac_common.mk
+++ config/pac_common.mk
@@ -73,25 +73,25 @@ CM_BUILD := $(BOARD)
 # Add PA release version
 PA_VERSION_MAJOR = 3
 PA_VERSION_MINOR = 6
-PA_VERSION_MAINTENANCE = 0
+PA_VERSION_MAINTENANCE = 8
 PA_PREF_REVISION = 1
 VERSION := $(PA_VERSION_MAJOR).$(PA_VERSION_MINOR)$(PA_VERSION_MAINTENANCE)
 PA_VERSION := pa_$(BOARD)-$(VERSION)-$(shell date +%0d%^b%Y-%H%M%S)
 
 # PAC version
-PAC_VERSION_MAJOR = 23
-PAC_VERSION_MINOR = 1
+PAC_VERSION_MAJOR = alpha
+PAC_VERSION_MINOR = build
 PAC_VERSION_MAINTENANCE = 0
 PAC_VERSION := $(PAC_VERSION_MAJOR).$(PAC_VERSION_MINOR).$(PAC_VERSION_MAINTENANCE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pac.version=$(PAC_VERSION) \
-    ro.pacrom.version=$(BOARD)_PAC_JB_4.3-v$(PAC_VERSION)_$(shell date +%Y%m%d-%H%M%S) \
+    ro.pacrom.version=pac_$(BOARD)_4.3-$(PAC_VERSION)_$(shell date +%Y%m%d-%H%M%S) \
     ro.modversion=$(PA_VERSION) \
     ro.pa.family=$(PA_CONF_SOURCE) \
     ro.pa.version=$(VERSION) \
     ro.papref.revision=$(PA_PREF_REVISION) \
-    ro.aokp.version=$(BOARD)_jb-mr1_milestone-1
+    ro.aokp.version=$(BOARD)_jb-mr1_milestone-2
 
 # Setup OTA with goo.im
 PRODUCT_PROPERTY_OVERRIDES += \
