427842e6d4342c09a6b728a59b0423f9e9f2badd
faust93 <monumentum@gmail.com>
2013-08-06 16:21:38 +0400

    LG Optimus G E975 added
    Change-Id: I43c646c6524062e7e0c4729c0b7e144c3d136753

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -120,6 +120,7 @@ add_lunch_combo pac_p970-userdebug
 add_lunch_combo pac_primoc-userdebug
 add_lunch_combo pac_primou-userdebug
 # LGE
+add_lunch_combo pac_e975-userdebug
 add_lunch_combo pac_p760-userdebug
 add_lunch_combo pac_p880-userdebug
 add_lunch_combo pac_p930-userdebug
