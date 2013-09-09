c5bb57036f79b443fcc68bdcca27c9a60627b795
hernan <hernanderes34@gmail.com>
2013-09-09 15:36:16 +0200

    E610 support
    Change-Id: I50bb0f5c4ac60fa2e6ff266b653b540e2986a93e

---
+++ products/pac_e610.mk
@@ -0,0 +1,30 @@
+# Check for target product
+ifeq (pac_e610,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_mdpi
+
+# AOKP device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/celox-common
+
+# PAC boot logo
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/common/bootlogo/pac_logo_320x480.rle:root/logo.rle
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/mdpi/bootanimation.zip:system/media/bootanimation.zip
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/lge/e610/cm.mk)
+
+PRODUCT_NAME := pac_e610
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
