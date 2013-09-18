1812d5afcd227cce2dfe7ec20eaf72ec7b356def
c457 <android.c357@gmail.com>
2013-09-17 23:27:59 -0500

    Minor changes My xda name is not c457 Epicmtd will not boot with pac animation Remove epicmtd from nightlies and add fascinate Epimtd use my vendor until lpm charging works Add fascinate thread Remove useless logo.rle for both epicmtd and fascinatemtd Fascinate uses 8x bootanimation device takes too long to boot
    Change-Id: Id7351bcd103f146ec2306eb0c7fd34e6d2403dc2

--- products/pac_fascinatemtd.mk
+++ products/pac_fascinatemtd.mk
@@ -15,13 +15,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/fascinatemtd
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/hdpi_480x800
 $(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-mask_samsung-hdpi.png frameworks/base/core/res/assets/images/android-logo-mask.png)
 
-# PAC boot logo
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/logo.rle
-
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+    vendor/pac/prebuilt/hdpi/8x/bootanimation.zip:system/media/bootanimation.zip
 
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
