ecf091581eaa54b124f84120f201fe1f5d516210
Joao <simoesj90@gmail.com>
2013-09-18 02:36:02 -0500

    Tweaks + Fixes
    Change-Id: Ie9bb66222a1912489697939034d735b4d4998156

--- products/pac_steelhead.mk
+++ products/pac_steelhead.mk
@@ -2,17 +2,14 @@
 ifeq (pac_steelhead,$(TARGET_PRODUCT))
 
 # OVERLAY_TARGET adds overlay asset source
-OVERLAY_TARGET := pa_nav_tvdpi
+OVERLAY_TARGET := pa_manta
 
 # AOKP device overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/tuna
-
-# PAC device overlay
-$(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-mask_samsung-xhdpi.png frameworks/base/core/res/assets/images/android-logo-mask.png)
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common_tablet
 
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
+    vendor/pac/prebuilt/xxhdpi/bootanimation.zip:system/media/bootanimation.zip
 
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
