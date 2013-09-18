8bc61d6ae519144d8797661bad819a562e179ad3
UtkarshGupta <utkarsh.eminem@gmail.com>
2013-09-18 14:37:58 +0530

    Preserve Xposed Framework Files
    This will Preserve Xposed Framework Files (/system/bin/app_process) while installing so that users don't have to reinstall it everytime they flash a nightly.
    
    PS2: Fixed White Spaces
    
    Change-Id: I848646390cc87834ad75a73709c0cf6046e00ce4

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -3,7 +3,7 @@
 # Backup and restore addon /system files
 #
 
-export C=/tmp/backupdir
+export C=/tmp/backup
 export S=/system
 
 # Preserve /system/addon.d in /tmp/addon.d
@@ -19,6 +19,25 @@ restore_addon_d() {
   rm -rf /tmp/addon.d/
 }
 
+# Backup Xposed Framework (bin/app_process)
+xposed_backup()
+{
+	if [ -f /system/bin/app_process.orig ]
+		then
+			cp /system/bin/app_process /tmp/backup/
+	fi
+}
+
+# Restore Xposed Framework (bin/app_process)
+xposed_restore()
+{
+	if [ -f /tmp/backup/app_process ]
+		then
+			mv /system/bin/app_process /system/bin/app_process.orig
+			cp /tmp/backup/app_process /system/bin/
+	fi
+}
+
 # Execute /system/addon.d/*.sh scripts with $1 parameter
 run_stage() {
 for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
@@ -29,12 +48,14 @@ done
 case "$1" in
   backup)
     mkdir -p $C
+    xposed_backup
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
     run_stage post-backup
   ;;
   restore)
+    xposed_restore
     run_stage pre-restore
     run_stage restore
     run_stage post-restore
