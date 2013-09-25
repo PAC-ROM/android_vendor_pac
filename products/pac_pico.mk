4f60f513dcff80d896211cc206a6547231e75505
Arnav Gosain <nolinuxnoparty@gmail.com>
2013-09-25 20:44:17 +0200

    HTC Pico: Cleanup
    Change-Id: I79a3d92b27aa5d4da67f657e64aa97e8c5e7d5ed

--- products/pac_pico.mk
+++ products/pac_pico.mk
@@ -14,8 +14,8 @@ $(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-ma
 # PAC boot logo
 PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/common/bootlogo/pac_logo_320x480.rle:root/PICO.rle
-
-# Bootanimation
+	
+# Copy bootanimation
 PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/mdpi/bootanimation.zip:system/media/bootanimation.zip
 
