1d8c5ad81bdcda4d2808e478a7412bd35fb2523c
krislibaeer <krislibaeer@gmail.com>
2013-10-15 11:30:41 +0200

    add adds and removes for Galaxy S Plus and Galaxy W
    Change-Id: I98d43a770f9c2706852c59aa017bdd2fca51a66a

--- products/pac_ariesve.mk
+++ products/pac_ariesve.mk
@@ -30,4 +30,8 @@ $(call inherit-product, device/samsung/ariesve/cm.mk)
 
 PRODUCT_NAME := pac_ariesve
 
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+
 endif
