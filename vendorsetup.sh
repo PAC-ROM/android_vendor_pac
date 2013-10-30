7b2b8d35d6dad7c5905e666f6baef2116da99a72
Brian Jolly <bkjolly77@gmail.com>
2013-10-30 13:36:04 +0100

    Motre Motos
    Change-Id: I947dcd1f09d1b22e62d1c0b7ffa9349076d9332d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -47,7 +47,9 @@ add_lunch_combo pac_vs920-userdebug
 # Motorola
 add_lunch_combo pac_edison-userdebug
 add_lunch_combo pac_ghostatt-userdebug
+add_lunch_combo pac_ghostspr-userdebug
 add_lunch_combo pac_ghosttmo-userdebug
+add_lunch_combo pac_ghostusc-userdebug
 add_lunch_combo pac_ghostvzw-userdebug
 add_lunch_combo pac_mb886-userdebug
 add_lunch_combo pac_maserati-userdebug
