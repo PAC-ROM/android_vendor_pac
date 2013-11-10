afe358ffe4bd4b9104e6e09a305025e525140b61
jon hey <haze028@gmail.com>
2013-11-09 20:06:57 -0500

    Add E973
    Change-Id: I9dfc6d7a8bba75ab1dc03f5bb44d41df8bd2af19

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -40,6 +40,7 @@ add_lunch_combo pac_u8860-userdebug
 add_lunch_combo pac_u8800pro-userdebug
 # LGE
 add_lunch_combo pac_d802-userdebug
+add_lunch_combo pac_e973-userdebug
 add_lunch_combo pac_e975-userdebug
 add_lunch_combo pac_ls970-userdebug
 add_lunch_combo pac_p760-userdebug
