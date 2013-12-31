764d2611d654adce807d7b5f2aa32e0d033baa30
Papa-Smurf151 <cbarlan@gmail.com>
2013-12-31 18:40:01 -0500

    Bring up Devices to 4.4
    Change-Id: I269e8226448c664cda810114df9fa14b6b87ba4d

---
+++ products/pac_a700.mk
@@ -0,0 +1,33 @@
+# Check for target product
+ifeq (pac_a700,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_a700
+
+# AOKP device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common_tablet
+
+# PAC device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/ldpi
+
+# PAC boot logo
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/common/bootlogo/pac_logo_1920x1200.rle:root/logo.rle
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/xxhdpi/bootanimation.zip:system/media/bootanimation.zip
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/acer/a700/cm.mk)
+
+PRODUCT_NAME := pac_a700
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
