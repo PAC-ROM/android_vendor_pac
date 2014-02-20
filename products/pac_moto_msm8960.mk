a12fd0f674f39e68b760a666a9558f12d7d5e87d
timpohladthomas <timpohladthomas@gmail.com>
2014-02-20 13:17:59 -0500

    Prepare for unified moto_msm8960 build
    Change-Id: I2d820913d216378c837a06af5ab477b021ccc341

--- products/pac_moto_msm8960.mk
+++ products/pac_moto_msm8960.mk
@@ -1,11 +1,11 @@
 # Check for target product
-ifeq (pac_vanquishgsm,$(TARGET_PRODUCT))
+ifeq (pac_moto_msm8960,$(TARGET_PRODUCT))
 
 # OVERLAY_TARGET adds overlay asset source
 OVERLAY_TARGET := pa_nav_xhdpi
 
 # AOKP device overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/vanquish
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/xhdpi
 
 # PAC boot logo
 PRODUCT_COPY_FILES += \
@@ -19,8 +19,8 @@ PRODUCT_COPY_FILES += \
 include vendor/pac/config/pac_common.mk
 
 # Inherit CM device configuration
-$(call inherit-product, device/motorola/xt925/cm.mk)
+$(call inherit-product, device/motorola/moto_msm8960/cm.mk)
 
-PRODUCT_NAME := pac_vanquishgsm
+PRODUCT_NAME := pac_moto_msm8960
 
 endif
