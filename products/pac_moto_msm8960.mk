89071aa017596c62093fe93ef8027bfe7f9441d9
timpohladthomas <timpohladthomas@gmail.com>
2014-02-25 12:48:33 -0500

    Change to hdpi images...
    In order for all devices to have PAC images we have to use the hdpi images, while they will be small on the xhdpi devices it is the only way to make them all have PAC images
    
    Change-Id: Ib9dd62900c9c519f84928823bb09b12b0ee9fbd2

--- products/pac_moto_msm8960.mk
+++ products/pac_moto_msm8960.mk
@@ -2,18 +2,18 @@
 ifeq (pac_moto_msm8960,$(TARGET_PRODUCT))
 
 # OVERLAY_TARGET adds overlay asset source
-OVERLAY_TARGET := pa_nav_xhdpi
+OVERLAY_TARGET := pa_nav_hdpi
 
 # AOKP device overlay
 #PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/xhdpi
 
 # PAC boot logo
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/bootlogo/pac_logo_720x1280.rle:root/logo.rle
+    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/logo.rle
 
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/720x1280/bootanimation.zip:system/media/bootanimation.zip
+    vendor/pac/prebuilt/540x960/bootanimation.zip:system/media/bootanimation.zip
 
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
