1dd85b656d7c1f07abfe87070aa97a8a655d63bd
José Adolfo Galdámez <josegalre@gmail.com>
2014-03-10 22:01:47 -0600

    Other Fix Bootanimation for ruby
    Change-Id: I8e2606c8def81453799b46a050b8ce937878c369

--- products/pac_ruby.mk
+++ products/pac_ruby.mk
@@ -13,7 +13,7 @@ PRODUCT_COPY_FILES += \
 
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/960x540/bootanimation.zip:system/media/bootanimation.zip
+    vendor/pac/prebuilt/540x960/bootanimation.zip:system/media/bootanimation.zip
 
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
