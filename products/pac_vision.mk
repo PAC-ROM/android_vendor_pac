90a98d7e414010a0e45408d5e0b6742f884958b2
MarcoMarinho <marcomarinho12@gmail.com>
2014-03-21 18:44:27 +0000

    Clean Up for HTC Desire Z
    Change-Id: I0eb931ab55b8bce1a1fb8a042de068b091e676d0

--- products/pac_vision.mk
+++ products/pac_vision.mk
@@ -23,4 +23,8 @@ $(call inherit-product, device/htc/vision/cm.mk)
 
 PRODUCT_NAME := pac_vision
 
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+
 endif
