7616f21334c3e4d7d89551506f98f52df54b39b6
WildfireDEV <wildfiredev@gmail.com>
2014-03-22 14:41:45 +1100

    Fix up Vivid files
    Change-Id: I6920c330d5068eb0185228bdbcacb10f8e146557

--- products/pac_holiday.mk
+++ products/pac_holiday.mk
@@ -16,4 +16,8 @@ $(call inherit-product, device/htc/holiday/cm.mk)
 
 PRODUCT_NAME := pac_holiday
 
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+
 endif
