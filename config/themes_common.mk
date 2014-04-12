9ed3f80dc2f1ec77c7816d4335f87929b3b163c6
Lokesh Chamane <lokesh.c703@gmail.com>
2014-04-11 22:25:03 -0400

    Revert "Use our own Theme Manager files"
    This reverts commit bc8ddb475b278ba61677328f593ed897a1fb63d6.
    
    Change-Id: I1cd84af68fc5e5fd7a788da00d4ccf36e10cb74a

--- config/themes_common.mk
+++
@@ -1,8 +0,0 @@
-# T-Mobile theme engine
-PRODUCT_PACKAGES += \
-    com.tmobile.themes \
-    ThemeManager \
-    ThemeChooser
-
-PRODUCT_COPY_FILES += \
-    vendor/pac/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
