dd6ddd504c8fe854d890cc9ba65d962c0a1e6e7c
MallardDuck <dpock32509@gmail.com>
2014-05-08 12:40:19 -0400

    Add Pac device overlays for m7spr
    This fixes the network mode selection screen that has been an issue for us
    for ages.
    CM isn't merging this into their device tree yet, but we should do it here
    to fix our ROM and if they merge it later I will track theirs and remove
    the redundancy (that won't even cause any errors) once they merge.
    
    Change-Id: I0a9ee416776d66b8b7d21d0744de3a131011fcfc

--- products/pac_m7spr.mk
+++ products/pac_m7spr.mk
@@ -4,6 +4,9 @@ ifeq (pac_m7spr,$(TARGET_PRODUCT))
 # OVERLAY_TARGET adds overlay asset source
 OVERLAY_TARGET := pa_xxhdpi
 
+# PAC device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/device/m7spr
+
 # Add bluefa1con bootanim
 PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/1080x1920/bootanimation.zip:system/media/bootanimation.zip
