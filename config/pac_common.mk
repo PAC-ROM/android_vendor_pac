f364d52874646f1af35cb8784cfc7f2fb157c81c
Lokesh Chamane <lokesh.c703@gmail.com>
2014-05-12 02:15:56 -0400

    Disable/clean-up some un-needed stuff
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- config/pac_common.mk
+++ config/pac_common.mk
@@ -9,8 +9,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #    vendor/pac/prebuilt/common/apk/PacConsole.apk:system/app/PacConsole.apk
 
 #Copy Theme Apks
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/apk/PacX.apk:system/app/PacX.apk
+#PRODUCT_COPY_FILES += \
+#    vendor/pac/prebuilt/common/apk/PacX.apk:system/app/PacX.apk
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
@@ -18,96 +18,85 @@ PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/pac/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh
 
-# Using Custom ReleaseRool
-#TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := vendor/pac/overlay/build/tools/releasetools/ota_from_target_files
-
-# T-Mobile theme engine
-#include vendor/pa/config/themes_common.mk
-
 # Screen recorder
-PRODUCT_PACKAGES += \
-    libscreenrecorder \
-    ScreenRecorder
+#PRODUCT_PACKAGES += \
+#    libscreenrecorder \
+#    ScreenRecorder
 
 # init.d support
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/bin/sysinit:system/bin/sysinit \
-    vendor/pac/prebuilt/common/etc/helpers.sh:system/etc/helpers.sh \
-    vendor/pac/prebuilt/common/etc/init.d.cfg:system/etc/init.d.cfg \
-    vendor/pac/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check \
-    vendor/pac/prebuilt/common/etc/init.d/01zipalign:system/etc/init.d/01zipalign \
-    vendor/pac/prebuilt/common/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
-    vendor/pac/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
-    vendor/pac/prebuilt/common/etc/init.d/05freemem:system/etc/init.d/05freemem \
-    vendor/pac/prebuilt/common/etc/init.d/06removecache:system/etc/init.d/06removecache \
-    vendor/pac/prebuilt/common/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
-    vendor/pac/prebuilt/common/etc/init.d/09cron:system/etc/init.d/09cron \
-    vendor/pac/prebuilt/common/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
-    vendor/pac/prebuilt/common/etc/init.d/11battery:system/etc/init.d/11battery \
-    vendor/pac/prebuilt/common/etc/init.d/12touch:system/etc/init.d/12touch \
-    vendor/pac/prebuilt/common/etc/init.d/13minfree:system/etc/init.d/13minfree \
-    vendor/pac/prebuilt/common/etc/init.d/14gpurender:system/etc/init.d/14gpurender \
-    vendor/pac/prebuilt/common/etc/init.d/15sleepers:system/etc/init.d/15sleepers \
-    vendor/pac/prebuilt/common/etc/init.d/16journalism:system/etc/init.d/16journalism \
-    vendor/pac/prebuilt/common/etc/init.d/17sqlite3:system/etc/init.d/17sqlite3 \
-    vendor/pac/prebuilt/common/etc/init.d/18wifisleep:system/etc/init.d/18wifisleep \
-    vendor/pac/prebuilt/common/etc/init.d/19iostats:system/etc/init.d/19iostats \
-    vendor/pac/prebuilt/common/etc/init.d/20setrenice:system/etc/init.d/20setrenice \
-    vendor/pac/prebuilt/common/etc/init.d/21tweaks:system/etc/init.d/21tweaks \
-    vendor/pac/prebuilt/common/etc/init.d/24speedy_modified:system/etc/init.d/24speedy_modified \
-    vendor/pac/prebuilt/common/etc/init.d/25loopy_smoothness_tweak:system/etc/init.d/25loopy_smoothness_tweak \
-    vendor/pac/prebuilt/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
-    vendor/pac/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf
+#PRODUCT_COPY_FILES += \
+#    vendor/pac/prebuilt/common/bin/sysinit:system/bin/sysinit \
+#    vendor/pac/prebuilt/common/etc/helpers.sh:system/etc/helpers.sh \
+#    vendor/pac/prebuilt/common/etc/init.d.cfg:system/etc/init.d.cfg \
+#    vendor/pac/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check \
+#    vendor/pac/prebuilt/common/etc/init.d/01zipalign:system/etc/init.d/01zipalign \
+#    vendor/pac/prebuilt/common/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
+#    vendor/pac/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
+#    vendor/pac/prebuilt/common/etc/init.d/05freemem:system/etc/init.d/05freemem \
+#    vendor/pac/prebuilt/common/etc/init.d/06removecache:system/etc/init.d/06removecache \
+#    vendor/pac/prebuilt/common/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
+#    vendor/pac/prebuilt/common/etc/init.d/09cron:system/etc/init.d/09cron \
+#    vendor/pac/prebuilt/common/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
+#    vendor/pac/prebuilt/common/etc/init.d/11battery:system/etc/init.d/11battery \
+#    vendor/pac/prebuilt/common/etc/init.d/12touch:system/etc/init.d/12touch \
+#    vendor/pac/prebuilt/common/etc/init.d/13minfree:system/etc/init.d/13minfree \
+#    vendor/pac/prebuilt/common/etc/init.d/14gpurender:system/etc/init.d/14gpurender \
+#    vendor/pac/prebuilt/common/etc/init.d/15sleepers:system/etc/init.d/15sleepers \
+#    vendor/pac/prebuilt/common/etc/init.d/16journalism:system/etc/init.d/16journalism \
+#    vendor/pac/prebuilt/common/etc/init.d/17sqlite3:system/etc/init.d/17sqlite3 \
+#    vendor/pac/prebuilt/common/etc/init.d/18wifisleep:system/etc/init.d/18wifisleep \
+#    vendor/pac/prebuilt/common/etc/init.d/19iostats:system/etc/init.d/19iostats \
+#    vendor/pac/prebuilt/common/etc/init.d/20setrenice:system/etc/init.d/20setrenice \
+#    vendor/pac/prebuilt/common/etc/init.d/21tweaks:system/etc/init.d/21tweaks \
+#    vendor/pac/prebuilt/common/etc/init.d/24speedy_modified:system/etc/init.d/24speedy_modified \
+#    vendor/pac/prebuilt/common/etc/init.d/25loopy_smoothness_tweak:system/etc/init.d/25loopy_smoothness_tweak \
+#    vendor/pac/prebuilt/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
+#    vendor/pac/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf
 
 # Added xbin files
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/xbin/zip:system/xbin/zip \
-    vendor/pac/prebuilt/common/xbin/zipalign:system/xbin/zipalign
+#PRODUCT_COPY_FILES += \
+#    vendor/pac/prebuilt/common/xbin/zip:system/xbin/zip \
+#    vendor/pac/prebuilt/common/xbin/zipalign:system/xbin/zipalign
 
 #OmniRom Packages
-PRODUCT_PACKAGES += \
-    OmniSwitch \
+#PRODUCT_PACKAGES += \
+#    OmniSwitch \
 
 # PAC Packages
 PRODUCT_PACKAGES += \
-    GooglePacman \
+#    GooglePacman \
     PacConsole \
-    Paclauncher \
+#    Paclauncher \
     PacPapers
 
 # PAC Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/common
 
 # Copy over the changelog to the device
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
 #    vendor/pac/CHANGELOG.mkdn:system/etc/PAC-CHANGELOG.txt \
-    vendor/pac/CONTRIBUTORS.mkdn:system/etc/PAC-CONTRIBUTORS.txt
+#    vendor/pac/CONTRIBUTORS.mkdn:system/etc/PAC-CONTRIBUTORS.txt
 
 # AOKP Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common
-
-### PARANOID ###
-# PARANOID Packages
-#PRODUCT_PACKAGES += \
-#    HALO
+#PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common
 
 # ParanoidAndroid Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/common
-PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/$(TARGET_PRODUCT)
+#PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/common
+#PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/$(TARGET_PRODUCT)
 
 # Allow device family to add overlays and use a same prop.conf
-ifneq ($(OVERLAY_TARGET),)
-    PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/$(OVERLAY_TARGET)
-    PA_CONF_SOURCE := $(OVERLAY_TARGET)
-else
-    PA_CONF_SOURCE := $(TARGET_PRODUCT)
-endif
+#ifneq ($(OVERLAY_TARGET),)
+#    PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/$(OVERLAY_TARGET)
+#    PA_CONF_SOURCE := $(OVERLAY_TARGET)
+#else
+#    PA_CONF_SOURCE := $(TARGET_PRODUCT)
+#endif
 
 # ParanoidAndroid Proprietary
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
 #     vendor/pa/prebuilt/apk/ParanoidPreferences.apk:system/app/ParanoidPreferences.apk \
-    vendor/pac/prebuilt/pa/$(PA_CONF_SOURCE).conf:system/etc/paranoid/properties.conf \
-    vendor/pac/prebuilt/pa/$(PA_CONF_SOURCE).conf:system/etc/paranoid/backup.conf
+#    vendor/pac/prebuilt/pa/$(PA_CONF_SOURCE).conf:system/etc/paranoid/properties.conf \
+#    vendor/pac/prebuilt/pa/$(PA_CONF_SOURCE).conf:system/etc/paranoid/backup.conf
 
 # ParanoidAndroid Images
 # PA_IMAGE_FILES := $(wildcard vendor/pa/prebuilt/preferences/images/*.png)
@@ -115,9 +104,9 @@ PRODUCT_COPY_FILES += \
 #     $(foreach f,$(PA_IMAGE_FILES),$(f):system/etc/paranoid/preferences/images/$(notdir $(f)))
 
 # ParanoidAndroid Preferences
-PA_PREF_FILES := $(wildcard vendor/pac/prebuilt/pa/preferences/$(PA_CONF_SOURCE)/*.xml)
-PRODUCT_COPY_FILES += \
-    $(foreach f,$(PA_PREF_FILES),$(f):system/etc/paranoid/preferences/$(notdir $(f)))
+#PA_PREF_FILES := $(wildcard vendor/pac/prebuilt/pa/preferences/$(PA_CONF_SOURCE)/*.xml)
+#PRODUCT_COPY_FILES += \
+#    $(foreach f,$(PA_PREF_FILES),$(f):system/etc/paranoid/preferences/$(notdir $(f)))
 
 BOARD := $(subst pac_,,$(TARGET_PRODUCT))
 
