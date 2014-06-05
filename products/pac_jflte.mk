fc618ed1b862a69108870626cbb88cd00c6e2c3a
Lokesh Chamane <lokesh.c703@gmail.com>
2014-06-05 17:43:43 -0400

    Clean-up and add Torch overlay for Jflte
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- products/pac_jflte.mk
+++ products/pac_jflte.mk
@@ -16,8 +16,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/common/apk/QuickBoot.apk:system/app/QuickBoot.apk
 
-# include PAC common configuration
+# Include PAC common configuration
 include vendor/pac/config/pac_common.mk
+
+# Device Overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+	vendor/pac/overlay/pac/device/jflte
 
 # Inherit CM device configuration
 $(call inherit-product, device/samsung/jflte/cm.mk)
