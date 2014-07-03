6c3633d8e1df600267f1f5ebe9044962dbf0d325
luk <priv.luk@gmail.com>
2014-07-03 20:42:42 +0200

    VENDOR/PAC: Fix adds and rms for s2ve/s2vep
    Change-Id: Id5fdc16e672be7601ac94af29cb52f67f2ebecae

--- products/pac_s2ve.mk
+++ products/pac_s2ve.mk
@@ -23,4 +23,8 @@ $(call inherit-product, device/samsung/s2ve/cm.mk)
 
 PRODUCT_NAME := pac_s2ve
 
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+
 endif
