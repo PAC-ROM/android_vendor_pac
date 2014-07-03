5bd22210f5d44f0de8de8a406bd1b34d4716dc2c
shumash <shumash@comcast.net>
2014-07-03 13:16:29 -0600

    tenderloin: Change in dependencies to 3.0 kernel, change to hdpi
    Change-Id: I8dbec656defa2c09cf843c0cc4c5933bfc05587d

--- products/pac_tenderloin.mk
+++ products/pac_tenderloin.mk
@@ -2,10 +2,10 @@
 ifeq (pac_tenderloin,$(TARGET_PRODUCT))
 
 # OVERLAY_TARGET adds overlay asset source
-OVERLAY_TARGET := pa_mdpi
+OVERLAY_TARGET := pa_hdpi
 
 # PAC device overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/mdpi
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/hdpi
 
 # PAC boot logo
 PRODUCT_COPY_FILES += \
