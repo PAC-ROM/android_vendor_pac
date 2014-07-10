d5e1855dee5c965980a433ba4ac0ee5b816f668b
MaDc0wUSP <MaDc0w@undergroundz.org>
2014-07-10 22:59:59 +0200

    Official support for OnePlus One - Codename: bacon
    Change-Id: I9da22d450ff643c8674559ea23c85d3ec9926f28

---
+++ products/pac_bacon.mk
@@ -0,0 +1,19 @@
+# Check for target product
+ifeq (pac_bacon,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xxhdpi
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/1080x1920/bootanimation.zip:system/media/bootanimation.zip
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/oneplus/bacon/cm.mk)
+
+PRODUCT_NAME := pac_bacon
+
+endif
