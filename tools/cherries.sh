153439370090b9b2d766cb22ed51462ab9914431
shumash <shumash@comcast.net>
2014-07-22 15:03:43 -0600

    cherries.sh: Add capability to cherry-pick non-gerrit commits
    Adds code to cherries.sh to allow properly formatted patches for any commit
    from any code repository, local or online to any source folder in the build.
    
    Online code by Lukasz
    
    Change-Id: Ie612e026260e5c5d820c8372b864670cc5af6633

--- tools/cherries.sh
+++ tools/cherries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Preamble: Some repos outside of PAC may have commits we need that takes too
+#Preamble: Some repos outside of PAC may have commits we need that take too
 #  long to be merged. Adding these cherry-picks here can automate the process
 #  and makes it possible to add them to the nightlies
 #  currently supported gerrit accounts are:
@@ -9,17 +9,70 @@
 #    CM   - CyanogenMod
 #    PAC  - pac-rom
 #    PA   - Paranoid Android (AOSPA)
+#
+#  in addition, cherry-pick patch files can be created for commits that aren't
+#  available in the above gerrit accounts.
+#
+# PATCH CAPABILITY BY SHUMASH 2014-07-22
+# ONLINE OPTION BY LUKASZ 2014-07-27
+# case $device in
+#  <device_name>)
+#    add cherry-pick patches for non-gerrit commits like this:
+#      PATCH=<name of patch without extension>
+#      FOLDER=<name of folder in build tree where patch is to be applied>
+#      patch_it  # this function call is required for each patch file
+#        |
+#      <Repeat for each separate patch>
+#
+#    For gerrit commits, add the cherry-pick(s) in the form:
+#      cherries +=(GERRIT-COMMIT#_GERRIT-ACCOUNT)
+#     e.g. http://review.pac-rom.com/#/c/250/2/tools/cherries.sh would be cherries+=(250_PAC)
+#        |
+#      <Repeat for each separate cherry-pick>
+#
+#   ;;
+#  On-line patches can be called by specifying a URL as the PATCH variable and calling the
+#  patch_it function with the parameter 'true', i.e., patch_it true
+#
+#  Any patches stored in the vendor/pac/tool/patches folder must have a .patch extension
+#  with a prefix name that exactly matches the PATCH name.  See below for examples.
+#  All patches must be in the git format-patch email format for use by git am
+#  Create a patch for the latest or last n patches with this command
+#     git format-patch -n (-1 for the last commit, -3 for the last three, etc.)
+#  Go here for a good description of how to create patches:
+#  http://docs.moodle.org/dev/How_to_create_a_patch
+#  Additionally, you can create a patch file from a github commit by going into the URL address
+#  line in your browser and adding ".patch" at the end.  It will immediately be downloaded as 
+#  patch file.
+# ---------------------------------------------------------
 
 device=$1
+BASEDIR=$PWD
 
 # colors
-. ./vendor/pac/tools/colors
+. $BASEDIR/vendor/pac/tools/colors
 
-## Add commits for all devices here
-# Name of commit for maintenance purposes
-#cherries+=(279_PA)
+function patch_it {
+  cd $BASEDIR/${FOLDER}
+  if [ "$1" = "true" ]; then
+   curl ${PATCH} | git am
+  else
+   git am $BASEDIR/vendor/pac/tools/patches/${PATCH}.patch
+  fi
 
-# Add device specific commits here
+  if [ -e ".git/rebase-apply" ]
+  then
+    git am --abort
+  elif [ -e ".git/CHERRY_PICK_HEAD" ]
+  then
+    git cherry-pick --abort
+  fi
+  cd $BASEDIR
+}
+
+
+# Add device specific commits and patches here
+
 case $device in
     anzu | coconut | haida | hallon | iyokan | mango | satsuma | smultron | urushi)
         # Revert "wpa_supplicant_8 - Hostapd: Android related changes for sockets"
@@ -75,14 +128,26 @@ case $device in
     ;;
     tenderloin)
         # btservice/AdaperState: handle ENABLED_READY in OffState
-        cherries+=(54380_CM)
+        PATCH=13-11-25_btservice-AdaperState-handle-ENABLED_READY
+        FOLDER=packages/apps/Bluetooth
+        patch_it #add this function call for each patch
+        PATCH=libhardware_legacy_ath6kl_wifi
+        FOLDER=hardware/libhardware_legacy
+        patch_it #add this function call for each patch
     ;;
 
 esac
 
+if [ "$PATCH" != "" ]; then
+    echo -e ""
+    echo -e ${bldblu}"Completed patching required cherries "${txtrst}
+    echo -e ""
+
+fi
+
 if [ "$cherries" != "" ]; then
     echo -e ""
-    echo -e ${bldblu}"Picking the required cherries"${txtrst}
+    echo -e ${bldblu}"Now picking the required cherries"${txtrst}
     echo -e ""
     ./build/tools/repopick.py -b ${cherries[@]}
 fi
