099bea962271a6a877bbf2d05994cebe802f8284
shumash <shumash@comcast.net>
2014-07-22 15:03:43 -0600

    Add capability to cherry-pick non-gerrit commits
    Adds code to cherries.sh to allow properly formatted patches for any commit
    from any code repository, local or online to any source folder in the build
    
    Change-Id: Ie612e026260e5c5d820c8372b864670cc5af6633

--- tools/cherries.sh
+++ tools/cherries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Preamble: Some repos outside of PAC may have commits we need that takes too
+#Preamble: Some repos outside of PAC may have commits we need that take too
 #  long to be merged. Adding these cherry-picks here can automate the process
 #  and makes it possible to add them to the nightlies
 #  currently supported gerrit accounts are:
@@ -9,17 +9,84 @@
 #    CM   - CyanogenMod
 #    PAC  - pac-rom
 #    PA   - Paranoid Android (AOSPA)
+#
+#  in addition, cherry-pick patch files can be created for commits that aren't
+#  available in the above gerrit accounts.
+#
+# ADDED BY SHUMASH 2014-07-22
+#  add cherry-pick patches for non-gerrit commits like this:
+#  After the NON-GERRIT case statement:
+#
+#  <device_name>)
+#      PATCH=<name of patch without extension>
+#      FOLDER=<name of folder in build tree where patch is to be applied>
+#      patch_it  # this function call is required for each patch file
+#	|
+#      <Repeat for each separate patch>
+#   ;;
+#
+#  All patches must have a .patch extension and stored in the vendor/pac/tool/patches folder
+#  with a prefix name that exactly matches the PATCH name. 
+#  Create a patch for the latest or last n patches with this command  
+#     git format-patch -n
+#  Go here for a good description of how to create patches:
+#  http://docs.moodle.org/dev/How_to_create_a_patch
+#  Additionally, you can create a patch file from a github commit by going into the URL address
+#  line in your browser and adding ".patch" at the end.  It will immediately be downloaded as 
+#  patch file.
+# ---------------------------------------------------------
 
 device=$1
+BASEDIR=$PWD
 
 # colors
-. ./vendor/pac/tools/colors
+. $BASEDIR/vendor/pac/tools/colors
+
+function patch_it {
+  cp -f $BASEDIR/vendor/pac/tools/patches/${PATCH}.patch $BASEDIR/${FOLDER}
+  cd $BASEDIR/${FOLDER}
+  git am ${PATCH}.patch
+
+  if [ -e *.patch ]
+  then
+    rm *.patch
+  fi
+  if [ -e ".git/rebase-apply" ]
+  then
+    git am --abort
+  elif [ -e ".git/CHERRY_PICK_HEAD" ]
+  then
+    git cherry-pick --abort
+  fi
+  cd $BASEDIR
+}
 
 ## Add commits for all devices here
+#
+# Insert NON-GERRIT cherry-picks below
+#
+case $device in
+    tenderloin)
+	# btservice/AdaperState: handle ENABLED_READY in OffState
+	PATCH=13-11-25_btservice-AdaperState-handle-ENABLED_READY
+	FOLDER=packages/apps/Bluetooth
+	patch_it #add this function call for each patch
+    ;;
+
+esac
+
+if [ "$PATCH" != "" ]; then
+    echo -e ""
+    echo -e ${bldblu}"Completed patching required cherries "${txtrst}
+    echo -e ""
+
+fi
+
+# Add device specific gerrit commits here
 # Name of commit for maintenance purposes
 #cherries+=(279_PA)
 
-# Add device specific commits here
+
 case $device in
     anzu | coconut | haida | hallon | iyokan | mango | satsuma | smultron | urushi)
         # Revert "wpa_supplicant_8 - Hostapd: Android related changes for sockets"
@@ -72,10 +139,6 @@ case $device in
     janice)
         # fix default colors for janice
         cherries+=(2154_PAC)
-    ;;
-    tenderloin)
-        # btservice/AdaperState: handle ENABLED_READY in OffState
-        cherries+=(54380_CM)
     ;;
 
 esac
