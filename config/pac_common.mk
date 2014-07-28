6ab533d469d0558de5b06e3d58c09297cce04ac7
Jiangyi <sam.andrew.jiang@gmail.com>
2014-07-28 18:07:09 -0400

    vendor/pac: Fix-ups for prebuilt Chromium
    - Make copying far more reliable, even on non-clean builds
    - Use $OUT in script in all cases, apparently get_build_var OUT_DIR isn't reliable.
    envsetup.sh, please -_-
    
    Change-Id: I27fa9bc7a043c32bcb29e7667e86b40226222a5a

--- config/pac_common.mk
+++ config/pac_common.mk
@@ -10,6 +10,11 @@ PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/pac/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh
 
+# Chromium Prebuilt
+ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
+-include prebuilts/chromium/$(TARGET_DEVICE)/chromium_prebuilt.mk
+endif
+
 # Screen recorder
 #PRODUCT_PACKAGES += \
 #    libscreenrecorder \
