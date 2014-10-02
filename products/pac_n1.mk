c75ea6dcd4febd0f76eca53ad4ab4aced261cf03
trialsrider1 <trialsrider1@gmail.com>
2014-10-01 19:26:12 -0600

    Adding Oppo N1
    Change-Id: I751a76ef4433c621a1c4e2080b04f3f05ee8093c

---
+++ products/pac_n1.mk
@@ -0,0 +1,20 @@
+# Check for target product
+ifeq (pac_n1,$(TARGET_PRODUCT))
+
+# PAC boot logo
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/common/bootlogo/pac_logo_1080x1920.rle:root/logo.rle
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/1080x1920/bootanimation.zip:system/media/bootanimation.zip
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/oppo/n1/cm.mk)
+
+PRODUCT_NAME := pac_n1
+
+endif
