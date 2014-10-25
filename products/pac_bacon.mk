46594c5a70e50b31f5e42a9c4953161431a5f4cb
Lokesh Chamane <lokesh.c703@gmail.com>
2014-10-24 21:32:26 -0400

    Bacon: Some overlay Change-Id: Id8aac966e413a3d1f59b4eab42081578c613ec59 Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- products/pac_bacon.mk
+++ products/pac_bacon.mk
@@ -8,6 +8,10 @@ PRODUCT_COPY_FILES += \
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
 
+# Device Overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/pac/overlay/device/bacon
+
 # Inherit CM device configuration
 $(call inherit-product, device/oneplus/bacon/cm.mk)
 
