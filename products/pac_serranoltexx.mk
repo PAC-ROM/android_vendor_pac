255239e0f69e037ef681e29c91a53ed7ef0db046
paloda <paloda09@gmail.com>
2014-10-28 17:29:55 +0100

    add support for Samsung galaxy SIV Mini,serranoltexx & serrano3gxx
    Change-Id: I7bdcfed5ad47661fc84fa4e98c99fd68b56e041c

---
+++ products/pac_serranoltexx.mk
@@ -0,0 +1,24 @@
+# Check for target product
+ifeq (pac_serranoltexx,$(TARGET_PRODUCT))
+
+# PAC boot logo
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/common/bootlogo/pac_logo_540x960.rle:root/logo.rle
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/540x960/bootanimation.zip:system/media/bootanimation.zip
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/samsung/serranoltexx/cm.mk)
+
+PRODUCT_NAME := pac_serranoltexx
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
