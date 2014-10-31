d855daa9f51b8232cca4a4b98ba8a2eb54b90339
paloda <paloda09@gmail.com>
2014-10-31 09:35:24 +0100

    add support S IV MINI
    Change-Id: I649ca3c9900d25389864c0754d4e2a86c9e0f24a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -92,6 +92,9 @@ add_lunch_combo pac_t0lteatt-userdebug
 add_lunch_combo pac_t0ltetmo-userdebug
 add_lunch_combo pac_venturi-userdebug
 add_lunch_combo pac_ypg1-userdebug
+add_lunch_combo pac_serranoltexx-userdebug
+add_lunch_combo pac_serrano3gxx-userdebug
+add_lunch_combo pac_serranodsdd-userdebug
 # Sony
 add_lunch_combo pac_amami-userdebug
 add_lunch_combo pac_honami-userdebug
