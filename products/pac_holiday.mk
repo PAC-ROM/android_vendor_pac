c367b525f86a719b9e47935f6465d4143d078f79
WildfireDEV <wildfiredev@gmail.com>
2014-11-08 08:32:21 +1100

    Fix bootanimation for HTC Vivid
    Change-Id: I3f7a55e90a7f6cde87bb1e26dacc087a9d8dfb7b

--- products/pac_holiday.mk
+++ products/pac_holiday.mk
@@ -3,7 +3,7 @@ ifeq (pac_holiday,$(TARGET_PRODUCT))
 
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/960x540/bootanimation.zip:system/media/bootanimation.zip
+    vendor/pac/prebuilt/540x960/bootanimation.zip:system/media/bootanimation.zip
 
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
