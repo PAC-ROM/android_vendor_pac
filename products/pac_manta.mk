c314ca79aaedefeeecc9adf4c6aa04bf17564f8b
volle <christoph.vollmer.85@gmail.com>
2014-11-24 11:00:49 +0100

    Add manta to LP
    PS2: rebase
    Change-Id: I28f3d13dafb166aaff191819a74f29d4b48a68e0

---
+++ products/pac_manta.mk
@@ -0,0 +1,16 @@
+# Check for target product
+ifeq (pac_manta,$(TARGET_PRODUCT))
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/1080x1920/bootanimation.zip:system/media/bootanimation.zip
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/samsung/manta/cm.mk)
+
+PRODUCT_NAME := pac_manta
+
+endif
