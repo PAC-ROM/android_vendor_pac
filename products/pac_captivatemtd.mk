67ed8f51b3bbcd7f87677b67dde61348d7eb0740
Lokesh Chamane <lokesh.c703@gmail.com>
2014-12-05 00:35:33 -0500

    Clean-up makefile for captivamtemtd and jflte
    Change-Id: I6d29330e205503d84a2ea8e8b94ba6efb725c2e7
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- products/pac_captivatemtd.mk
+++ products/pac_captivatemtd.mk
@@ -1,10 +1,6 @@
 # Check for target product
 ifeq (pac_captivatemtd,$(TARGET_PRODUCT))
 
-# PAC boot logo
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/logo.rle
-
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/480x800/bootanimation.zip:system/media/bootanimation.zip
