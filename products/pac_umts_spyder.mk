0d1f206b14b5a85ace93095d9214ee948fd7e85e
José Adolfo Galdámez <josegalre@gmail.com>
2014-12-14 17:47:58 -0600

    vendor: cleanup product makefiles
    * also remove some tabs
    
    Change-Id: Icaf3b1740b630c8df211e82d5fc6713a7c21ac77

--- products/pac_umts_spyder.mk
+++ products/pac_umts_spyder.mk
@@ -1,14 +1,10 @@
 # Check for target product
 ifeq (pac_umts_spyder,$(TARGET_PRODUCT))
 
-# PAC boot logo
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/logo.rle
-
-# Copy bootanimation
+# Bootanimation
 PAC_BOOTANIMATION_NAME := 480
 
-# include PAC common configuration
+# Include PAC common configuration
 include vendor/pac/config/pac_common.mk
 
 # Inherit CM device configuration
