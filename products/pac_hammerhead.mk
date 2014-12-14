4deaa7bdfffb68ec673cf9d117d332fd556b5309
José Adolfo Galdámez <josegalre@gmail.com>
2014-12-14 14:43:33 -0600

    vendor: cleanup product makefiles
    * also remove some tabs
    
    Change-Id: Icaf3b1740b630c8df211e82d5fc6713a7c21ac77

--- products/pac_hammerhead.mk
+++ products/pac_hammerhead.mk
@@ -1,14 +1,14 @@
 # Check for target product
 ifeq (pac_hammerhead,$(TARGET_PRODUCT))
 
-# Copy bootanimation
+# Bootanimation
 PAC_BOOTANIMATION_NAME := 1080
 
 # Copy QuickBoot.apk
 PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/common/apk/QuickBoot.apk:system/app/QuickBoot.apk
 
-# include PAC common configuration
+# Include PAC common configuration
 include vendor/pac/config/pac_common.mk
 
 # Inherit CM device configuration
--- products/pac_i9100.mk
+++ products/pac_i9100.mk
@@ -1,17 +1,10 @@
 # Check for target product
 ifeq (pac_i9100,$(TARGET_PRODUCT))
 
-# PAC device overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/hdpi_480x800
-
-# PAC boot logo
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/logo.rle
-
-# Copy bootanimation
+# Bootanimation
 PAC_BOOTANIMATION_NAME := 480
 
-# include PAC common configuration
+# Include PAC common configuration
 include vendor/pac/config/pac_common.mk
 
 # Inherit CM device configuration
