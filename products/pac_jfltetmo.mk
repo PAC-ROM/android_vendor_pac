a28d4a238948579d11eb40452fc8a7d552e16d4c
Lokesh Chamane <lokesh.c703@gmail.com>
2014-12-14 18:17:24 -0800

    More updates
    - Remove old maintainers
    - Take over all the jflte
    - Update QuickBoot.apk
    
    Change-Id: I836408cb69114a89f7c7f80836730657b0385937
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- products/pac_jfltetmo.mk
+++ products/pac_jfltetmo.mk
@@ -7,14 +7,10 @@ PRODUCT_COPY_FILES += \
 
 # Copy QuickBoot.apk
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/apk/QuickBoot.apk:system/app/QuickBoot.apk
+    vendor/pac/prebuilt/common/apk/QuickBoot.apk:system/app/QuickBoot/QuickBoot.apk
 
 # Include PAC common configuration
 include vendor/pac/config/pac_common.mk
-
-# Device Overlays
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/pac/overlay/device/jfltetmo
 
 # Inherit CM device configuration
 $(call inherit-product, device/samsung/jfltetmo/cm.mk)
