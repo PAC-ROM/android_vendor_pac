ba718bec148ff7e450c48664ce3b41dd2e115aed
José Adolfo Galdámez <josegalre@gmail.com>
2014-12-14 21:37:17 -0600

    vendor: cleanup product makefiles
    * super clean xD
    
    Change-Id: Icaf3b1740b630c8df211e82d5fc6713a7c21ac77

--- products/pac_jactivelte.mk
+++ products/pac_jactivelte.mk
@@ -1,23 +1,14 @@
 # Check for target product
 ifeq (pac_jactivelte,$(TARGET_PRODUCT))
 
-# PAC boot logo
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/bootlogo/pac_logo_1080x1920.rle:root/logo.rle
-
-# Copy bootanimation
+# Bootanimation
 PAC_BOOTANIMATION_NAME := 1080
 
-# Copy QuickBoot.apk
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/apk/QuickBoot.apk:system/app/QuickBoot.apk
+# QuickBoot
+PAC_USE_QUICKBOOT := true
 
 # Include PAC common configuration
 include vendor/pac/config/pac_common.mk
-
-# Device Overlays
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/pac/overlay/device/jactivelte
 
 # Inherit CM device configuration
 $(call inherit-product, device/samsung/jactivelte/cm.mk)
--- products/pac_s2ve.mk
+++ products/pac_s2ve.mk
@@ -1,23 +1,18 @@
 # Check for target product
 ifeq (pac_s2ve,$(TARGET_PRODUCT))
 
-# PAC boot logo
-PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/logo.rle
-
-# Copy bootanimation
+# Bootanimation
 PAC_BOOTANIMATION_NAME := 480
 
-# include PAC common configuration
+# Use Add-Remove
+PAC_USE_ADDREMOVE := true
+
+# Include PAC common configuration
 include vendor/pac/config/pac_common.mk
 
 # Inherit CM device configuration
 $(call inherit-product, device/samsung/s2ve/cm.mk)
 
 PRODUCT_NAME := pac_s2ve
-
-# Update local_manifest.xml
-GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
-GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
 
 endif
