c7e2ed5d468b2fce4035c54cdf9bec62f86f9b1d
José Adolfo Galdámez <josegalre@gmail.com>
2014-12-14 21:37:17 -0600

    vendor: cleanup product makefiles
    * super clean xD
    
    Change-Id: Icaf3b1740b630c8df211e82d5fc6713a7c21ac77

--- products/pac_p705.mk
+++ products/pac_p705.mk
@@ -1,19 +1,18 @@
 # Check for target product
 ifeq (pac_p705,$(TARGET_PRODUCT))
 
-# Copy bootanimation
+# Bootanimation
 PAC_BOOTANIMATION_NAME := 480
 
-# include PAC common configuration
+# Use Add-Remove
+PAC_USE_ADDREMOVE := true
+
+# Include PAC common configuration
 include vendor/pac/config/pac_common.mk
 
 # Inherit CM device configuration
 $(call inherit-product, device/lge/p705/cm.mk)
 
 PRODUCT_NAME := pac_p705
-
-# Update local_manifest.xml
-GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
-GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
 
 endif
