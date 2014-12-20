991618fda36f680306cee4b72d4d9c754480289e
demonslayer12349 <demonslayer12349@gmail.com>
2014-12-20 00:19:35 -0500

    Please add support for LG optimus L90 (US variant) codenamed w7
    Change-Id: Ib2c5fc4cd18f000e87d1cda045e726ce40f63117

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -54,6 +54,7 @@ add_lunch_combo pac_p705-userdebug
 add_lunch_combo pac_v500-userdebug
 add_lunch_combo pac_vs980-userdebug
 add_lunch_combo pac_vs985-userdebug
+add_lunch_combo pac_w7-userdebug
 # Motorola
 add_lunch_combo pac_condor-userbebug
 add_lunch_combo pac_falcon-userdebug
