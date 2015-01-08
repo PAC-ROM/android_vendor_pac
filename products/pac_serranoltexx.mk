89413d4180f502397fb1e05bc0a577574e6a2cd5
paloda <paloda09@gmail.com>
2015-01-08 23:18:02 +0100

    Add s4 mini to pac 5.0
    Change-Id: Ib1cb192b9630f4a9de33c644ca52de276545cb31

---
+++ products/pac_serranoltexx.mk
@@ -0,0 +1,18 @@
+# Check for target product
+ifeq (pac_serranoltexx,$(TARGET_PRODUCT))
+
+# Bootanimation
+PAC_BOOTANIMATION_NAME := 540
+
+# QuickBoot
+PAC_USE_QUICKBOOT := true
+
+# Include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/samsung/serranoltexx/cm.mk)
+
+PRODUCT_NAME := pac_serranoltexx
+
+endif
