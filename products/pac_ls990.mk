feb56110e39fc02fb17769204aed00fc30e45c44
MallardDuck <dpock32509@gmail.com>
2015-01-17 22:33:49 -0500

    Pac-out Sprint G3 (ls990) for LoliPop
    Change-Id: I545c9fea92b16e423f37833fa73f75a5b3ff3a57

---
+++ products/pac_ls990.mk
@@ -0,0 +1,15 @@
+# Check for target product
+ifeq (pac_ls990,$(TARGET_PRODUCT))
+
+# Bootanimation
+PAC_BOOTANIMATION_NAME := 1080
+
+# Include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/lge/ls990/cm.mk)
+
+PRODUCT_NAME := pac_ls990
+
+endif
