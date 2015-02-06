ed5f08ba74cc7e94a4c215b31015a400b87b92cb
JoseGalRe <josegalre@gmail.com>
2015-02-05 18:07:02 -0600

    vendor: temporarily remove csp, cri and ucs variants of jf
    Change-Id: I4870620a4ca7c6ae31a7fd0db83a1492a966ef37

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -73,11 +73,8 @@ add_lunch_combo pac_i9500-userdebug
 add_lunch_combo pac_jactivelte-userdebug
 add_lunch_combo pac_jflteatt-userdebug
 add_lunch_combo pac_jfltecan-userdebug
-add_lunch_combo pac_jfltecri-userdebug
-add_lunch_combo pac_jfltecsp-userdebug
 add_lunch_combo pac_jfltespr-userdebug
 add_lunch_combo pac_jfltetmo-userdebug
-add_lunch_combo pac_jflteusc-userdebug
 add_lunch_combo pac_jfltevzw-userdebug
 add_lunch_combo pac_jfltexx-userdebug
 add_lunch_combo pac_l900-userdebug
