dd6179ac0217eefa4a2283571621b5ecdc468798
José Adolfo Galdámez <josegalre@gmail.com>
2015-02-16 21:57:57 -0600

    vendor: add PAC_LOW_RAM_DEVICE
    * Remove live wallpapers based on:
      https://gerrit.omnirom.org/#/c/11993
      https://gerrit.omnirom.org/#/c/12046
    
    * Reference for low-ram devices: https://source.android.com/devices/tech/low-ram.html
    
    * Use dlmalloc instead of jemalloc, it performs better on low-ram devices
    
    * Reduce background apps limit to 16 on low-tier devices
    
    * Set max background services to 8 on low-tier devices
    
    * Disable bootanimation cached texture, saves memory used at boot
    
    * Disable atlas services on low-ram targets
    
    Flag: PAC_LOW_RAM_DEVICE := true
    
    Add this in your pac_$device.mk
    
    Change-Id: I50f43774695847ccec03d7743b6454937a168307

--- config/pac_common.mk
+++ config/pac_common.mk
@@ -25,6 +25,22 @@ PRODUCT_NAME := $(TARGET_PRODUCT)
 # Set the board version
 CM_BUILD := $(BOARD)
 
+# Lower RAM devices
+ifeq ($(PAC_LOW_RAM_DEVICE),true)
+MALLOC_IMPL := dlmalloc
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    config.disable_atlas=true \
+    dalvik.vm.jit.codecachesize=0 \
+    dalvik.vm.jit.codecachesize=0 \
+    persist.sys.force_highendgfx=true \
+    ro.config.low_ram=true \
+    ro.config.low_ram=true \
+    ro.config.max_starting_bg=8 \
+    ro.sys.fw.bg_apps_limit=16
+endif
+
 # ROMStats Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pacstats.url=http://stats.pac-rom.com \
