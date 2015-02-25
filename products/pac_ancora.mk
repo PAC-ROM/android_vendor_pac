0b0cd3b8e6e87be4d3411128edf78263bf75d867
khusika <khusikadhamar@gmail.com>
2015-02-25 13:03:24 +0700

    Fix Add support for Ancora & Ariesve
    Change-Id: If2a70b5b6944d2f0603f38eea38a7a1084fd28c4

---
+++ products/pac_ancora.mk
@@ -0,0 +1,14 @@
+# Check for target product
+ifeq (pac_ancora,$(TARGET_PRODUCT))
+
+# Bootanimation
+PAC_BOOTANIMATION_NAME := 480
+
+# include PAC common configuration
+PRODUCT_NAME := pac_ancora
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/samsung/ancora/cm.mk)
+
+endif
