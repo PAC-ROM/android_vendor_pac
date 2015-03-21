4cc1e45e63716114dabaf3cdf89342b1c72e3508
JoseGalRe <josegalre@gmail.com>
2015-03-21 14:20:39 -0600

    vendor: clean our scripts files WIP
    * Add our licence
    * Add option to not use ccache
    * Now using grep -c instead of grep|wc
    * Now use $(..) instead of legacy `..`
    * Fixed some ouble quote to prevent globbing and word splitting
    
    p16: (apparently): copyleft text pursuant to Gnu.
    p17: headers: fixup build-pac.sh and whitespace derps.
    p18:  fixup usage of USE_PREBUILT_CHROMIUM and USE_CCACHE
    p19: putting back @jaaga's changes I inadvertently overwrote
    
    Change-Id: I27a0e0f96d71e4dbe15844bf0150831c685dee98

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -1,6 +1,21 @@
 #!/sbin/sh
 #
-# Backup and restore addon /system files
+# backuptool.sh: Backup and restore addon /system files
+# Copyright (C) 2015 PAC-Roms Project
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
 export C=/tmp/backupdir
@@ -12,101 +27,101 @@ cp -f /tmp/install/bin/backuptool.functions /tmp
 
 # Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {
-  mkdir -p /tmp/addon.d/
-  cp -a /system/addon.d/* /tmp/addon.d/
-  chmod 755 /tmp/addon.d/*.sh
+    mkdir -p /tmp/addon.d/
+    cp -a /system/addon.d/* /tmp/addon.d/
+    chmod 755 /tmp/addon.d/*.sh
 }
 
 # Restore /system/addon.d in /tmp/addon.d
 restore_addon_d() {
-  cp -a /tmp/addon.d/* /system/addon.d/
-  rm -rf /tmp/addon.d/
+    cp -a /tmp/addon.d/* /system/addon.d/
+    rm -rf /tmp/addon.d/
 }
 
 # Proceed only if /system is the expected major and minor version
 check_prereq() {
-  retval=1
-  ver=$(awk "/ro.pac.version=($V)/ {print \"LP\"}" /system/build.prop)
-  case "$ver" in
-    LP)
-      echo "Found compatible version, backing up files."
-      ;;
-    *)
-      echo "Not backing up files from incompatible version: $V"
-      retval=0
-      ;;
-  esac
-  return $retval
+    retval=1
+    ver=$(awk "/ro.pac.version=($V)/ {print \"LP\"}" /system/build.prop)
+    case "$ver" in
+        LP)
+            echo "Found compatible version, backing up files."
+        ;;
+         *)
+            echo "Not backing up files from incompatible version: $V"
+            retval=0
+        ;;
+    esac
+    return $retval
 }
 
 check_blacklist() {
-  if [ -f /system/addon.d/blacklist ];then
-      ## Discard any known bad backup scripts
-      cd /$1/addon.d/
-      for f in *sh; do
-          s=$(md5sum $f | awk {'print $1'})
-          grep -q $s /system/addon.d/blacklist && rm -f $f
-      done
-  fi
+    if [ -f /system/addon.d/blacklist ];then
+        ## Discard any known bad backup scripts
+        cd /"$1"/addon.d/
+        for f in *sh; do
+            s=$(md5sum "$f" | awk '{print $1}')
+            grep -q "$s" /system/addon.d/blacklist && rm -f "$f"
+        done
+    fi
 }
 
 check_whitelist() {
-  found=0
-  if [ -f /system/addon.d/whitelist ];then
-      ## forcefully keep any version-independent stuff
-      cd /$1/addon.d/
-      for f in *sh; do
-          s=$(md5sum $f | awk {'print $1'})
-          grep -q $s /system/addon.d/whitelist
-          if [ $? -eq 0 ]; then
-              found=1
-          else
-              rm -f $f
-          fi
-      done
-  fi
-  return $found
+    found=0
+    if [ -f /system/addon.d/whitelist ];then
+        ## forcefully keep any version-independent stuff
+        cd /"$1"/addon.d/
+        for f in *sh; do
+            s=$(md5sum "$f" | awk '{print $1}')
+            grep -q "$s" /system/addon.d/whitelist
+            if [ $? -eq 0 ]; then
+                found=1
+            else
+                rm -f "$f"
+            fi
+        done
+    fi
+    return $found
 }
 
 # Execute /system/addon.d/*.sh scripts with $1 parameter
 run_stage() {
-for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
-    $script $1
-done;
+    for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
+        $script "$1"
+    done;
 }
 
 case "$1" in
-  backup)
-    mkdir -p $C
-    if check_prereq; then
-        if check_whitelist system; then
-            exit 127
-        fi
-    fi
-    check_blacklist system
-    preserve_addon_d
-    run_stage pre-backup
-    run_stage backup
-    run_stage post-backup
-  ;;
-  restore)
-    if check_prereq; then
-        if check_whitelist tmp; then
-            exit 127
-        fi
-    fi
-    check_blacklist tmp
-    run_stage pre-restore
-    run_stage restore
-    run_stage post-restore
-    restore_addon_d
-    rm -rf $C
-    rm -rf /data/data/android.pacstats
-    sync
-  ;;
-  *)
-    echo "Usage: $0 {backup|restore}"
-    exit 1
+    backup)
+       mkdir -p $C
+       if check_prereq; then
+           if check_whitelist system; then
+               exit 127
+           fi
+       fi
+       check_blacklist system
+       preserve_addon_d
+       run_stage pre-backup
+       run_stage backup
+       run_stage post-backup
+    ;;
+    restore)
+       if check_prereq; then
+          if check_whitelist tmp; then
+             exit 127
+          fi
+       fi
+       check_blacklist tmp
+       run_stage pre-restore
+       run_stage restore
+       run_stage post-restore
+       restore_addon_d
+       rm -rf $C
+       rm -rf /data/data/android.pacstats
+       sync
+    ;;
+    *)
+       echo "Usage: $0 {backup|restore}"
+       exit 1
 esac
 
 exit 0
