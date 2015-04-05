f39f7b4f4aa57127f859c0ebe3b51007a0934d83
JoseGalRe <josegalre@gmail.com>
2015-04-05 00:43:47 -0600

    vendor: add Samsung Galaxy S4 US Cellular (jflteusc) to pac-5.0
    Change-Id: I73c70fa02dfc08663583ff4d58443c9c3996eb07

---
+++ products/pac_jflteusc.mk
@@ -0,0 +1,13 @@
+# Check for target product
+ifeq (pac_jflteusc,$(TARGET_PRODUCT))
+
+# Bootanimation
+PAC_BOOTANIMATION_NAME := 1080
+
+# Include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/samsung/jflteusc/cm.mk)
+
+endif
