1d683b342d4c78c68543c89310c33681181a493a
José Adolfo Galdámez <josegalre@pac-rom.com>
2015-05-12 19:32:52 -0600

    Merge pac/pac-5.1 into pac-5.1-twrp
    Conflicts:
    	.gitreview
    	docs/PrepareForBuildLinux.md
    	docs/PrepareForBuildMac.md
    
    Change-Id: I7c22d29e8204e5ebd8ecf34d9b55f0a5930a57e1
    Signed-off-by: José Adolfo Galdámez <josegalre@pac-rom.com>

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -20,7 +20,7 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V="LP"
+export V="LP-MR1"
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
