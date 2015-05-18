2da09d0481306dd29451b6c4d9edaaeb72b5eda5
Eliminater74 <eliminater74@gmail.com>
2015-05-17 20:05:27 -0400

    Support SM-T800 and LG-D851
    Change-Id: I74dd83438bdcee71c489b49d867eda28cae9618e
    Samsung Galaxy S Tab 10.5 (T800) Codename: chagallwifi
    and LG G3 (D851) Support

---
+++ products/pac_d851.mk
@@ -0,0 +1,13 @@
+# Check for target product
+ifeq (pac_d851,$(TARGET_PRODUCT))
+
+# Bootanimation
+PAC_BOOTANIMATION_NAME := 1440
+
+# Include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/lge/d851/cm.mk)
+
+endif
