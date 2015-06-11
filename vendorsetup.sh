958afec505bd960cf027114c89a2a726c6e77e9c
kunalkene1797 <kunalkene1797@gmail.com>
2015-06-11 14:31:36 +0530

    Add Official Support For Google Android One Sprout4 and Sprout8 Devices
    Change-Id: I3892ee066bd1f34e60f8ad5e0141ee083934ef6c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,6 +12,9 @@ add_lunch_combo pac_tf700t-userdebug
 # Barnes & Noble
 add_lunch_combo pac_acclaim-userdebug
 add_lunch_combo pac_ovation-userdebug
+# Google
+add_lunch_combo pac_sprout4-userdebug
+add_lunch_combo pac_sprout8-userdebug
 # HP
 add_lunch_combo pac_tenderloin-userdebug
 # HTC
