3ba6d2074c9c2a7451cb57c042b34bb166efb2d9
José Adolfo Galdámez <josegalre@pac-rom.com>
2015-06-28 00:34:29 -0600

    vendor: Major cleanup!
    Change-Id: I9bf3be7643704688fef4e200a0290b9797809139
    Signed-off-by: José Adolfo Galdámez <josegalre@pac-rom.com>

--- tools/extras.sh
+++ tools/extras.sh
@@ -155,20 +155,6 @@ case $device in
         # Allow using Classic WebView
         cherries+=(56054_CM)
     ;;
-    ariesve | ancora)
-        #MemoryHeapBase: ifdef for gingerbread/froyo compatibility
-        cherries+=(58227_CM)
-        #Overlay support for legacy camera libs
-        cherries+=(58228_CM)
-        #sensorservice: Add legacy sensors fusion.
-        cherries+=(81684_CM)
-        #native: add flag to disable legacy sensors fusion
-        cherries+=(36732_CM)
-        #linker: restore prelink support
-        cherries+=(78604_CM)
-        #Revert "Revert "Reenable support for non-PIE executables""
-        cherries+=(79136_CM)
-    ;;
     tenderloin)
         # btservice/AdaperState: handle ENABLED_READY in OffState
         PATCH=tenderloin_13-11-25_btservice-AdaperState-handle-ENABLED_READY
