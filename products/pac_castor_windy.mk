557eb2daf9d36249f8317620c3be05df0db93cbf
Aaron Kable <aaronkable@gmail.com>
2015-07-23 15:29:53 +0000

    Add castor and castor_windy
    *Not Ready
    
    Change-Id: I579b3b6eb68a447c08b42d8870b84aa8aad1f4dc

---
+++ products/pac_castor_windy.mk
@@ -0,0 +1,13 @@
+# Check for target product
+ifeq (pac_castor_windy,$(TARGET_PRODUCT))
+
+# Bootanimation
+PAC_BOOTANIMATION_NAME := 1200
+
+# Include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/sony/castor_windy/cm.mk)
+
+endif
