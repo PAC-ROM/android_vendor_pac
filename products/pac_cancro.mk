5adaa84179da3347f4ac2c9f972116cdb85f4242
pranay <pranaypingale07@gmail.com>
2015-08-28 13:28:57 +0530

    Cancro bring up
    Change-Id: Ic086745143adc11c7ed1730498847bb01ef61b85
    Signed-off-by: pranay <pranaypingale07@gmail.com>

---
+++ products/pac_cancro.mk
@@ -0,0 +1,14 @@
+# Check for target product
+ifeq (pac_cancro,$(TARGET_PRODUCT))
+
+# Bootanimation
+PAC_BOOTANIMATION_NAME := 1080
+
+# Include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/xiaomi/cancro/cm.mk)
+#
+
+endif
