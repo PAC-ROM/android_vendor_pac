5e2c5a0c153c966973a7b4f860678547ad186db0
Shreps <shr3ps@gmail.com>
2015-10-07 10:25:24 +0200

    Osprey bring up ! Moto G 2015 3rd Gen
    Change-Id: Ifc0460c4d7912ae03b1553b9de92a0d570025ae6

---
+++ products/pac_osprey.mk
@@ -0,0 +1,13 @@
+# Check for target product
+ifeq (pac_osprey,$(TARGET_PRODUCT))
+
+# Bootanimation
+PAC_BOOTANIMATION_NAME := 720
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/motorola/osprey/cm.mk)
+
+endif
