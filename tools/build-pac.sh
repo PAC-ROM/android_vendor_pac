35257c6083fd77211d765ee9b0eb2adb18973e11
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2015-10-20 00:53:05 -0400

    Update version
    Change-Id: I036b024bd28909aba4cb552e20373929bc776265

--- tools/build-pac.sh
+++ tools/build-pac.sh
@@ -63,7 +63,7 @@ usage() {
 
 
 # PAC version
-export PAC_VERSION_MAJOR="LP-MR1"
+export PAC_VERSION_MAJOR="MM"
 export PAC_VERSION_MINOR=""
 export PAC_VERSION_MAINTENANCE="Unofficial"
 # Acceptable maintenance versions are; Stable, Official, Nightly or Unofficial
