67735f135c02a50765d4b714627f94b45e71ce4e
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2015-10-28 19:05:24 -0400

    vendor: cleanup bootanimation logic
    Change-Id: Iad3bea31581a6cf6eaa84e177b571f5c4083ddd7
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- config/common_mini_tv.mk
+++ config/common_mini_tv.mk
@@ -8,8 +8,3 @@ include vendor/pac/config/pac_audio.mk
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Argon.ogg \
     ro.config.alarm_alert=Helium.ogg
-
-ifeq ($(PAC_BOOTANIMATION_NAME),)
-    PRODUCT_COPY_FILES += \
-        vendor/pac/prebuilt/common/media/bootanimation/800.zip:system/media/bootanimation.zip
-endif
