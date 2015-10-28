c1f6c23f8b777978dcc1238daa75adaf4c838b88
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2015-10-28 16:27:35 -0400

    vendor: updates
    Change-Id: I7608d2da27dcfd121cfc383c4ff70d029b70aa40
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- config/common.mk
+++ config/common.mk
@@ -17,12 +17,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
-# Bootanimation
-PRODUCT_COPY_FILES += vendor/pac/prebuilt/common/media/bootanimation/$(PAC_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
-
-# Kernel Adiutor
-PRODUCT_COPY_FILES += vendor/pac/prebuilt/common/app/KernelAdiutor/KernelAdiutor.apk:system/app/KernelAdiutor/KernelAdiutor.apk
-
 # PAC version
 PACVERSION := $(shell echo $(PAC_VERSION) | sed -e 's/^[ \t]*//;s/[ \t]*$$//;s/ /./g')
 BOARD := $(subst pac_,,$(TARGET_PRODUCT))
@@ -31,52 +25,6 @@ PRODUCT_NAME := $(TARGET_PRODUCT)
 
 # Set the board version
 PAC_BUILD := $(BOARD)
-
-# Lower RAM devices
-ifeq ($(PAC_LOW_RAM_DEVICE),true)
-MALLOC_IMPL := dlmalloc
-TARGET_BOOTANIMATION_TEXTURE_CACHE := false
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    config.disable_atlas=true \
-    dalvik.vm.jit.codecachesize=0 \
-    persist.sys.force_highendgfx=true \
-    ro.config.low_ram=true \
-    ro.config.max_starting_bg=8 \
-    ro.sys.fw.bg_apps_limit=16
-endif
-
-# ROMStats Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.pacstats.url=http://www.pac-rom.com/pages/submit.php \
-    ro.pacstats.name=PAC-ROM \
-    ro.pacstats.version=$(PACVERSION) \
-    ro.pacstats.tframe=1
-
-# TWRP Recovery
-ifeq ($(RECOVERY_VARIANT),twrp)
-    ifeq ($(PAC_MAKE),recoveryimage)
-        BOARD_SEPOLICY_IGNORE += external/sepolicy/domain.te
-        BOARD_SEPOLICY_DIRS += vendor/pac/sepolicy/twrp
-        BOARD_SEPOLICY_UNION += domain.te init.te recovery.te
-    endif
-endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.root_access=0
-
-# Disable ADB authentication and set root access to Apps and ADB
-ifeq ($(DISABLE_ADB_AUTH),true)
-    ADDITIONAL_DEFAULT_PROPERTIES += \
-        ro.adb.secure=3 \
-        persist.sys.root_access=3
-endif
-
-# Add and Remove
-ifeq ($(PAC_USE_ADDREMOVE),true)
-    GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
-    GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
-endif
 
 PRODUCT_BRAND ?= pacrom
 
@@ -112,6 +60,51 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
+# Lower RAM devices
+ifeq ($(PAC_LOW_RAM_DEVICE),true)
+MALLOC_IMPL := dlmalloc
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+PRODUCT_PROPERTY_OVERRIDES += \
+    config.disable_atlas=true \
+    dalvik.vm.jit.codecachesize=0 \
+    persist.sys.force_highendgfx=true \
+    ro.config.low_ram=true \
+    ro.config.max_starting_bg=8 \
+    ro.sys.fw.bg_apps_limit=16
+endif
+
+# ROMStats Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.pacstats.url=http://www.pac-rom.com/pages/submit.php \
+    ro.pacstats.name=PAC-ROM \
+    ro.pacstats.version=$(PACVERSION) \
+    ro.pacstats.tframe=1
+
+# TWRP Recovery
+ifeq ($(RECOVERY_VARIANT),twrp)
+ifeq ($(PAC_MAKE),recoveryimage)
+BOARD_SEPOLICY_IGNORE += external/sepolicy/domain.te
+BOARD_SEPOLICY_DIRS += vendor/pac/sepolicy/twrp
+BOARD_SEPOLICY_UNION += domain.te init.te recovery.te
+endif
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.root_access=0
+
+# Disable ADB authentication and set root access to Apps and ADB
+ifeq ($(DISABLE_ADB_AUTH),true)
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.adb.secure=3 \
+    persist.sys.root_access=3
+endif
+
+# Add and Remove
+ifeq ($(PAC_USE_ADDREMOVE),true)
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+endif
+
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
@@ -136,6 +129,10 @@ PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 endif
 
+# Kernel Adiutor
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/common/app/KernelAdiutor/KernelAdiutor.apk:system/app/KernelAdiutor/KernelAdiutor.apk
+
 # PAC-specific init file
 PRODUCT_COPY_FILES += \
     vendor/pac/prebuilt/common/etc/init.local.rc:root/init.pac.rc
@@ -155,25 +152,17 @@ PRODUCT_COPY_FILES += \
 # T-Mobile theme engine
 include vendor/pac/config/themes_common.mk
 
-# Required PAC packages
+# PAC packages
 PRODUCT_PACKAGES += \
     Launcher3 \
-    Development \
-    BluetoothExt \
-    Profiles
-
-# Optional PAC packages
-PRODUCT_PACKAGES += \
-    VoicePlus \
-    Basic \
-    libemoji \
-    Terminal
-
-# Custom PAC packages
-PRODUCT_PACKAGES += \
     AudioFX \
+    Basic \
+    BluetoothExt \
+    Development \
     Eleven \
+    libemoji \
     LockClock \
+    Terminal
 
 # Platform Library
 PRODUCT_PACKAGES += \
