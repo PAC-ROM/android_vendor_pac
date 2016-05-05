41f999b30f745593f5696a999d77124336254fd7
Gary Kipnis <gkipnis@cyngn.com>
2016-05-05 15:25:00 -0700

    Added DataUsageProvider to the build
    Change-Id: Ia167198b82a9b41ed93e59048a55d0e1da7f0512

--- config/common.mk
+++ config/common.mk
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \
     CMSettingsProvider \
     ExactCalculator \
     LiveLockScreenService \
-    WeatherProvider
+    WeatherProvider \
+    DataUsageProvider
 
 # Exchange support
 PRODUCT_PACKAGES += \
